85b909c637d5ffb4b198d5cdc8874ca4bef73453
caooss <rafael.r.l.pavan@gmail.com>
2020-10-18 15:32:58 -0300

    Foram criados os arquivos para uma calculadora em Java Script

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Calculadora</title>
+        <script src="./js/calc.js" charset="utf-8"></script>
+        <link rel="stylesheet" href="./css/estilo.css">
+    </head>
+    <body>
+
+    </body>
+</html>
